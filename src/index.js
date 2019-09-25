3bd220f6e85243a3b33ef976873f0b0fff7d1437
Chaim Finkelman <toChaim@gmail.com>
2019-09-24 22:40:33 -0700

    tic tac toe setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const NavBar = ()=>(<nav>
+  <ul><a href="#">Home</a><li><a href="#">Tic Tack Toe</a></li></ul>
+</nav>);
+const Display = ()=>{
+  return (
+    <div className="display">
+      <h1>Next player: X</h1>
+      <button>Controles</button>
+    </div>);
+};
+const Board = ()=>{
+  return (
+    <div className="board">
+      <div className="square"><button>1</button></div>
+      <div className="square"><button>2</button></div>
+      <div className="square"><button>3</button></div>
+      <div className="square"><button>4</button></div>
+      <div className="square"><button>5</button></div>
+      <div className="square"><button>6</button></div>
+      <div className="square"><button>7</button></div>
+      <div className="square"><button>8</button></div>
+      <div className="square"><button>9</button></div>
+    </div>
+  );
+};
+const Game = ()=>{
+  return (
+    <div className="game">
+      <div className="title">
+        <h1>Tic Tac Toe</h1>
+      </div>
+      <Display />
+      <Board />
+    </div>);
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => (<div className="container">
+  <NavBar />
+  <Game />
+</div>);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
