7af62fddd0e10a885fcc120ae2905cde330c7ed0
Chaim Finkelman <toChaim@gmail.com>
2019-10-03 11:29:54 -0700

    got row and col number adjustable

--- src/index.js
+++ src/index.js
@@ -3,26 +3,24 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+// Functions
 Array.prototype.radomize = () => this.forEach((v,i,a)=>console.log(v,i));
-const Board = ({row, col, peices} = {row:3, col:3, peices:[]})=>{
-  let squares = new Array(row * col).fill(1).map((v,i)=>(<div className="square"><button>{i}</button></div>));
-  return (<div className="board">{squares}</div>);
-};
-const Games = {
-  'Tic Tac Toe': {
-    Board: Board
-  },
-  'Memory': {
-    Board: Board
-  }
-};
+
+// Attoms
 const Btn = ({handleClick, buttonText, className})=>(
   <button className={className + ' btn'} 
     onClick={() => handleClick(buttonText)}>
     {buttonText}
   </button>);
+
+// Molecules
+const Board = ({row, col, peices} = {row:3, col:3, peices:[]})=>{
+  console.log(row, col);
+  let squares = new Array(row * col).fill(1).map((v,i)=>(<div className="square"><button>{i}</button></div>));
+  return (<div className="board">{squares}</div>);
+};
+
 const NavBar = ({Games, handleClick, game})=>{
-  console.log(Object.keys(Games), game);
   let gamesList = Object.keys(Games).map( g => (<li><Btn className={game===g? 'selected':'not'} handleClick={handleClick} buttonText={g} /></li>));
   return (<nav>
     <ul>
@@ -31,6 +29,19 @@ const NavBar = ({Games, handleClick, game})=>{
     </ul>
   </nav>);
 };
+
+// Data
+const Games = {
+  'Tic Tac Toe': {
+    Board: Board
+  },
+  'Memory': {
+    row: 4,
+    col: 4,
+    Board: Board
+  }
+};
+
 const Display = ({row, col, setRow, setCol})=>{
   // let inputs = Object.keys(gameState).map(v => (<li>
   //   <label>{v}:<input name={v} type="number" value={'gameState[v]'} onChange={()=>console.log('()=>setGameState(gameState)')} /></label>
@@ -38,21 +49,22 @@ const Display = ({row, col, setRow, setCol})=>{
   return (
     <div className="display">
       <ul>Game Sate:
-        <li>row: <input type="number" value={row} /></li>
-        <li>col: <input type="number" value={col} /></li>
+        <li>row: <input type="number" value={row} onChange={e => setRow(e.target.value)}/></li>
+        <li>col: <input type="number" value={col} onChange={e => setCol(e.target.value)}/></li>
       </ul>
     </div>);
 };
 const Game = ({game})=>{
-  const board = Games[game].Board();
+  let [row, setRow] = useState(Games[game]['row'] || 3);
+  let [col, setCol] = useState(Games[game]['col'] || 3);
 
   return (
     <div className="game">
       <div className="title">
         <h1>{game}</h1>
       </div>
-      <Display row={3} col={3} />
-      {board}
+      <Display row={row} setRow={setRow} col={col} setCol={setCol}/>
+      <Board row={row} col={col} />
     </div>);
 };
 const App = () => {
