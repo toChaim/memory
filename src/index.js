6139430159d3d39174b46e9f2b871d304311bd3c
Chaim Finkelman <toChaim@gmail.com>
2019-10-05 23:32:08 -0700

    lost

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // Functions
+
 Array.prototype.radomize = () => {this.forEach((v,i,a)=>console.log(v,i)); return this;};
 
 // Attoms
@@ -17,7 +18,7 @@ const MemorySquar = ({val, handleClick, selected})=>{
   return <div className={selected?'square selected':'square'}><button onClick={()=>handleClick(val)}>{selected?val:''}</button></div>;
 };
 // Molecules
-const Board = ({row, col, squares} = {row:3, col:3, peices:[]})=>{
+const Board = ({row, col, squares} = {row:3, col:3, squares:[]})=>{
   document.documentElement.style.setProperty('--row', row);
   document.documentElement.style.setProperty('--col', col);
 
