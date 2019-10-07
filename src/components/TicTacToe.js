484ddd0214290300b2c99e7182b7618bb46c0472
Chaim Finkelman <toChaim@gmail.com>
2019-10-07 12:46:21 -0700

    building TicTacToe

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Board from './Board';
 
+const Square = ({selected, handleClick, val, index}) => (
+  <div className={selected?'square selected':'square'}>
+    <button onClick={()=>handleClick(index)}>{val}</button>
+  </div>);
+
 export default ()=>{
+  const [gameState, setGameState] = useState({
+    peices: new Array(9).fill(''),
+    state: 'Player X\'s turn.'
+  });
+
+  const handleClick = index => {
+    if(gameState.peices[index]){
+      const state = gameState.state;
+      setGameState({state: 'BAD MOVE'});
+      setTimeout(()=>{
+        setGameState({state: state});
+      },1000);
+    }
+    else{
+      //handle game state
+    }
+  };
   return (<div className="game">Tic Tac Toe</div>);
 };
