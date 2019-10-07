ee4bb82dd8f7ec27057a46e441501101881d1c73
Chaim Finkelman <toChaim@gmail.com>
2019-10-07 13:54:04 -0700

    read to start tutorial

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -2,27 +2,39 @@ import React, {useState} from 'react';
 import Board from './Board';
 
 const Square = ({selected, handleClick, val, index}) => (
-  <div className={selected?'square selected':'square'}>
-    <button onClick={()=>handleClick(index)}>{val}</button>
-  </div>);
+  <button className="square" index={index}>
+    {val}
+  </button>);
 
 export default ()=>{
   const [gameState, setGameState] = useState({
     peices: new Array(9).fill(''),
-    state: 'Player X\'s turn.'
+    status: 'Player X\'s turn.'
   });
 
-  const handleClick = index => {
-    if(gameState.peices[index]){
-      const state = gameState.state;
-      setGameState({state: 'BAD MOVE'});
-      setTimeout(()=>{
-        setGameState({state: state});
-      },1000);
-    }
-    else{
-      //handle game state
-    }
-  };
-  return (<div className="game">Tic Tac Toe</div>);
+  // const handleClick = index => {
+  //   if(gameState.peices[index]){
+  //     const status = gameState.status;
+  //     setGameState({status: 'BAD MOVE'});
+  //     setTimeout(()=>{
+  //       setGameState({state: status});
+  //     },1000);
+  //   }
+  //   else{
+  //     //handle game state
+  //   }
+  // };
+
+  return (
+    <div className="game">
+      <div className="title">
+        <h1>Tic Tac Toe</h1>
+      </div>
+      <div className="display">{gameState.status}</div>
+      <Board row={3} col={3} squares={gameState.peices.map((v,i)=><Square val={v} index={i} />)}/>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>);
 };
