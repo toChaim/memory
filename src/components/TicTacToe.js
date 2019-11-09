5c060e9ee4a8fde70e9cbccffc8a2751abaa8a7b
Chaim Finkelman <toChaim@gmail.com>
2019-11-09 12:56:35 -0800

    square to function

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,60 +1,65 @@
 import React, {useState} from 'react';
-import Board from './Board';
 
-import {deepCopy} from '../helperFunctions';
+const Square = () => (
+  <button className="square">
+    {/* TODO */}
+  </button>
+);
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square">
+//         {/* TODO */}
+//       </button>
+//     );
+//   }
+// }
 
-const getTurn = arr => arr.reduce((t,v)=> t += !!v, 0) % 2? 'O':'X';
-const getGameStatus = arr => {
-  for(let i = 0; i < 3; i++){
-    if(arr[i*3] && arr[i*3] === arr[i*3+1] &&  arr[i*3] === arr[i*3+2]){ return arr[i*3] + ' wins!!!'; }
-    if(arr[i] && arr[i] === arr[i + 3] && arr[i] === arr[i + 6]){ return arr[i] + ' wins!!!'; }
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
   }
-  if(arr[0] && arr[0] === arr[4] && arr[0] === arr[8]){ return arr[0] + ' wins!!!'; }
-  if(arr[2] && arr[2] === arr[4] && arr[2] === arr[6]){ return arr[2] + ' wins!!!'; }
-  let moves = arr.reduce((t,v)=> t + !!v,0)
-  if(moves === 9){ return 'Tie Game.'}
-  return `Player ${moves % 2? 'O':'X'}'s move.`;
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
 }
 
-const Square = ({handleClick, val, index}) => (
-  <button className="square" index={index} onClick={()=> handleClick(index)}>
-    {val}
-  </button>);
-
-const StartState = {
-  peices: new Array(9).fill(''),
-  status: 'Player X\'s turn.'
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ()=>{
-  const [gameState, setGameState] = useState(StartState);
-
-  const handleSquareClick = index => {
-    if(gameState.peices[index]){
-      const newState = {...deepCopy(gameState), status:'BAD MOVE'};
-      setGameState(newState);
-      setTimeout(()=>{
-        setGameState({...deepCopy(gameState), status: getGameStatus(gameState.peices)});
-      },1000);
-    }
-    else{
-      let newState = deepCopy(gameState);
-      newState.peices[index] = getTurn(gameState.peices);
-      newState.status = getGameStatus(newState.peices);
-      setGameState(newState);
-    }
-  };
-
-  const handleResetClick = () => setGameState(StartState);
-  return (
-    <div className="game">
-      <div className="title">
-        <h1>Tic Tac Toe</h1>
-      </div>
-      <div className="display">
-      {gameState.status}
-      <button onClick={handleResetClick}>Reset</button>
-      </div>
-      <Board row={3} col={3} squares={gameState.peices.map((v,i)=><Square val={v} index={i} key={i} handleClick={handleSquareClick}/>)}/>
-    </div>);
-};
+export default Game;
