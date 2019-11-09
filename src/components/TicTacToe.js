b19696f90935e723f4b5af9132befb8edddd1f4f
Chaim Finkelman <toChaim@gmail.com>
2019-11-09 13:00:33 -0800

    change board to function

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -5,46 +5,33 @@ const Square = () => (
     {/* TODO */}
   </button>
 );
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button className="square">
-//         {/* TODO */}
-//       </button>
-//     );
-//   }
-// }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
+const Board = () => {
+  const renderSquare = (i) => <Square />;
 
-  render() {
-    const status = 'Next player: X';
+  const status = 'Next player: X';
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 class Game extends React.Component {
   render() {
