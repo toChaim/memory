885ac93ed4a73d6886c2cb19920339f188f6bce1
Chaim Finkelman <toChaim@gmail.com>
2019-11-22 20:32:10 -0800

    add useGetComponentSize

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,3 +1,21 @@
+import React, { useLayoutEffect, useState, useRef } from 'react';
+
+const useGetComponentSize = () => {
+  const ref = useRef({});
+  const [dimensions, setDimensions] = useState({ x: 0, y: 0, width: window.innerWidth, height: window.innerHeight, top: 0, left: 0 });
+  const updateSize = () => {
+    if (ref.current) { setDimensions(ref.current.getBoundingClientRect()); }
+    else { setDimensions({ x: 0, y: 0, width: window.innerWidth, height: window.innerHeight, top: 0, left: 0 }); }
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', updateSize);
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  return [dimensions, ref];
+};
+
 function randomizeArray(arr){
   let res =  arr.map(v=>v);
   res.forEach((v,i,a)=>{
