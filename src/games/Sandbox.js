5639911ea109e599182f02421b95d1f1392150d3
Chaim Finkelman <toChaim@gmail.com>
2019-12-01 23:33:01 -0800

    test winbar for tictactoe

---
+++ src/games/Sandbox.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useComponentSize } from '../helperFunctions';
+import Line from '../Line';
+import Board from '../Board';
+
+// Usage
+function GetSize() {
+  const [boardSize, boardRef] = useComponentSize();
+  const squares = new Array(9).fill(1).map((cur,idx) => (<button className='square' key={idx} style={{background: 'transparent', position: 'relative'}}>{idx}</button>));
+  const extras = [<Line key={1} x1={0} y1={0} x2={(boardSize.width || 10) - 10} y2={(boardSize.height || 10) - 10} />];
+
+  return (
+    <div
+      className="game"
+      style={{
+        display: 'grid',
+        gridTemplateRows: '1fr 8fr',
+        border: 'none',
+      }}
+    >
+      <div className="game-info"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          fontSize: '24px',
+          fontWeight: 'bold',
+          border: 'none',
+          margin: 'auto',
+          width: '100%',
+        }}>{boardSize.width}px / {boardSize.height}px</div>
+      <Board boardRef={boardRef} squares={squares} extras={extras}/>
+    </div>
+  );
+}
+
+export default GetSize;
