7120afbec5fdcfd38104cf28e3b8bd7d5089ee0a
Chaim Finkelman <toChaim@gmail.com>
2019-12-01 23:31:06 -0800

    make board component

---
+++ src/Board.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Board = ({ row = 3, col = 3, squares, extras = [], boardRef } = { row: 3, col: 3 }) => (<div
+  ref={boardRef}
+  className="board"
+  style={{
+    position: 'relative',
+    display: 'grid',
+    gridTemplate: `repeat(${row}, 1fr) / repeat(${col}, 1fr)`,
+    gridGap: '5px',
+    padding: '5px',
+    border: '5px solid pink',
+    borderRadius: '5px',
+    backgroundColor: 'white',
+  }}
+>
+  {extras}
+  {squares}
+</div>
+);
+
+export default Board;
