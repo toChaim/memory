fed6b45fccdbf419c6a280d2d15f91e8d77575c4
Chaim Finkelman <toChaim@gmail.com>
2019-12-02 22:06:26 -0800

    get win lines working

--- src/games/TicTacToe.js
+++ src/games/TicTacToe.js
@@ -1,28 +1,9 @@
-import React, { useState } from 'react';
-import {useComponentSize} from '../helperFunctions';
+import React, {useState} from 'react';
+import { useComponentSize } from '../helperFunctions';
+import Line from '../Line';
+// import Board from '../Board';
 
-const Line = ({ x1, y1, x2, y2, stroke = 10, color = 'red' }) => {
-  let width = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5;
-  let left = (x1 + x2 - width - stroke) / 2;
-  console.log(width, left, (x1 + x2) / 2);
-
-  return (<div
-    className="line"
-    style={{
-      position: 'absolute',
-      border: `${stroke / 2}px solid ${color}`,
-      borderRadius: stroke + 'px',
-      transform: `rotate(${Math.atan((y2 - y1) / (x2 - x1)) * 180 / Math.PI}deg)`,
-      top: (y1 + y2 - stroke) / 2 + 'px',
-      left: left + 'px',
-      width: width + 'px',
-      // opacity: 0.5
-    }} />);
-};
-
-const Square = ({
-  idx, content, condition, handleClick,
-}) => (
+const Square = ({ idx, content, condition, handleClick }) => (
   <button
     id={`sqr${idx}`}
     className="square "
@@ -32,7 +13,7 @@ const Square = ({
       fontSize: '24px',
       fontWeight: 'bold',
       borderRadius: '5px',
-      border: '5px solid pink',
+      border: `5px solid ${condition? 'lightgreen':'pink'}`,
       background: 'transparent',
     }}
   >
@@ -73,35 +54,7 @@ const GameInfo = ({ message, handleReset }) => (
   </div>
 );
 
-const Board = ({ row = 3, col = 3, squares, extras } = { row: 3, col: 3}) => {
-  const [dimensions, ref] = useComponentSize();
-  extras = [(<div key={'winrow'} id={'winrow'} className="winbar" style={{
-    left: 0,
-    top: 0,
-    width: '1px',
-    height: '1px',
-    background: 'lightgreen',
-    position: 'absolute',
-    border: '5px solid blue'
-  }}>
-  </div>),
-  (<div key={'wincol'} id={'wincol'} className="winbar" style={{
-    left: dimensions.width - 20 + 'px',
-    top: dimensions.height - 20 + 'px',
-    width: '1px',
-    height: '1px',
-    background: 'lightgreen',
-    position: 'absolute',
-    border: '5px solid blue'
-  }}>
-  </div>)];
-
-  // extras = [
-  //   {id:'winrow', x1: 0, y1: 0},
-  //   {id:'wincol', x1: dimensions.width - dimensions.x, y1:dimensions.height - dimensions.y}];
-  // console.log(dimensions,extras);
-  // extras = extras.map(cur => (<WinBar id={cur.id} x1={cur.x1} y1={cur.y1} />));
-
+const Board = ({ row = 3, col = 3, squares, extras, boardRef } = { row: 3, col: 3}) => {
   const handleKeyPress = (event) => {
     if (document.activeElement.id.substr(0, 3) === 'sqr') {
       const oldIdx = Number(document.activeElement.id.substr(3));
@@ -122,7 +75,7 @@ const Board = ({ row = 3, col = 3, squares, extras } = { row: 3, col: 3}) => {
 
   return (
     <div
-      ref={ref}
+      ref={boardRef}
       className="board"
       onKeyDown={handleKeyPress}
       style={{
@@ -146,44 +99,58 @@ const Game = () => {
   const [values, setValues] = useState(new Array(9).fill('').map((cur) => ({ content: '', condition: '' })));
   const players = ['X', 'O'];
   const [turn, setTurn] = useState(0);
-  const [winType, setWinType] = useState([]);
-  const [boardDimensions, setBoardDimensions] = useState(null);
+  const [wins, setWins] = useState([]);
+  const [dimensions, ref] = useComponentSize();
 
   const move = (i) => {
     if (values[i].content || turn >= players.length) { return; }
     const newValues = values.map((cur, idx) => (i === idx ? { ...cur, content: players[turn] } : { ...cur }));
+    const newWins = wins.map(cur => {return {...cur}; });
     let newTurn = (turn + 1) % players.length;
+    
     for (let i = 0; i < 3; i++) {
       if (players[turn] === newValues[i * 3].content && players[turn] === newValues[i * 3 + 1].content && players[turn] === newValues[i * 3 + 2].content) {
         newTurn = players.length + turn;
-        
+        newWins.push( { key: 'row', x1: 1 / 9, y1: 1/6 + i/3, x2: 8/9, y2: 1/6 + i/3 } );
+        newValues[i*3].condition = newValues[i*3 + 1].condition = newValues[i*3 + 2].condition = 'win';
       }
       if (players[turn] === newValues[i].content && players[turn] === newValues[i + 3].content && players[turn] === newValues[i + 6].content) {
         newTurn = players.length + turn;
-        newValues[i].condition += '|';
-        newValues[i + 3].condition += '|';
-        newValues[i + 6].condition += '|';
+        newWins.push( { key: 'cols', x1: 1/6 + i/3, y1: 1 / 9, x2: 1/6 + i/3, y2: 8/9 } );
+        newValues[i].condition = newValues[i + 3].condition = newValues[i + 6].condition = 'win';
       }
     }
     if (players[turn] === newValues[0].content && players[turn] === newValues[4].content && players[turn] === newValues[8].content) {
       newTurn = players.length + turn;
-      newValues[0].condition += '/';
-      newValues[4].condition += '/';
-      newValues[8].condition += '/';
+      newWins.push({ key: 'slash', x1: 1 / 9, y1: 1 / 9, x2: 8/9, y2: 8/9 } );
+      newValues[0].condition = newValues[4].condition = newValues[8].condition = 'win';
     }
     if (players[turn] === newValues[2].content && players[turn] === newValues[4].content && players[turn] === newValues[6].content) {
       newTurn = players.length + turn;
-      newValues[2].condition += '\\';
-      newValues[4].condition += '\\';
-      newValues[6].condition += '\\';
+      newWins.push({ key: 'backslash', x1: 8/9, y1: 1/9, x2: 1/9, y2: 8/9 } );
+      newValues[2].condition = newValues[4].condition = newValues[6].condition = 'win';
     }
     if (newValues.filter((cur) => cur.content).length === 9 && turn < players.length) {
       newTurn = players.length * 2;
     }
 
+    setWins(newWins);
     setValues(newValues);
     setTurn(newTurn);
   };
+
+  const extras = wins.map(cur => {
+    let width = dimensions.width - 20;
+    let height = dimensions.height -20;
+
+    return (<Line 
+      key={cur.key}
+      x1={ width * cur.x1 + 5} // 1/3 for rows and slashes i + 0.5 for col
+      y1={ height * cur.y1 + 5} // 1/3 for cols and slashes i + 0.5 for row
+      x2={ width * cur.x2 + 5} 
+      y2={ height * cur.y2 + 5} 
+      color={'lightgreen'} />);
+  });
 
   const squares = values.map((cur, idx) => (
     <Square
@@ -198,9 +165,10 @@ const Game = () => {
   const handleReset = () => {
     setValues(new Array(9).fill('').map((cur) => ({ content: '', condition: '' })));
     setTurn(0);
+    setWins([]);
   };
 
-  const getMessage = () => {
+  const getMessage = (players, turn) => {
     if (players[turn]) { return `Player ${players[turn]}'s turn.`; }
     if (players[turn - players.length]) { return `${players[turn - players.length]} wins!!!`; }
     return 'Tie Game';
@@ -215,8 +183,8 @@ const Game = () => {
         border: 'none',
       }}
     >
-      <GameInfo message={getMessage(values, turn)} handleReset={handleReset} />
-      <Board squares={squares} extras={winType}/>
+      <GameInfo message={getMessage(players, turn)} handleReset={handleReset} />
+      <Board squares={squares} extras={extras} boardRef={ref}/>
     </div>
   );
 };
