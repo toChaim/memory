94c88527a54c8a4ddb92cd178dcf07b1a8a17c7f
Chaim Finkelman <toChaim@gmail.com>
2019-12-02 22:05:54 -0800

    move code to tictactoe

--- src/games/Sandbox.js
+++ src/games/Sandbox.js
@@ -3,10 +3,12 @@ import { useComponentSize } from '../helperFunctions';
 import Line from '../Line';
 import Board from '../Board';
 
+const Square = ({idx}) => (<button className='square' style={{ background: 'transparent', position: 'relative' }}>{idx}</button>);
+
 // Usage
 function GetSize() {
   const [boardSize, boardRef] = useComponentSize();
-  const squares = new Array(9).fill(1).map((cur,idx) => (<button className='square' key={idx} style={{background: 'transparent', position: 'relative'}}>{idx}</button>));
+  const squares = new Array(9).fill(1).map((cur,idx) => (<Square idx={idx} key={idx} />));
   const extras = [<Line key={1} x1={0} y1={0} x2={(boardSize.width || 10) - 10} y2={(boardSize.height || 10) - 10} />];
 
   return (
