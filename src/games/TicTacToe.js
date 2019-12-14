41b1fa543e7dd900063adf1b7d893afd7b94e668
Chaim Finkelman <toChaim@gmail.com>
2019-12-14 07:58:43 -0800

    use reducer

--- src/games/TicTacToe.js
+++ src/games/TicTacToe.js
@@ -21,6 +21,12 @@ const Square = ({ idx, content, condition, handleClick }) => (
   </button>
 );
 
+const getMessage = (players, turn) => {
+  if (players[turn]) { return `Player ${players[turn]}'s turn.`; }
+  if (players[turn - players.length]) { return `${players[turn - players.length]} wins!!!`; }
+  return 'Tie Game';
+};
+
 const GameInfo = ({ message, handleReset }) => (
   <div
     className="game-info"
@@ -55,11 +61,78 @@ const GameInfo = ({ message, handleReset }) => (
 );
 
 const Game = () => {
-  const [values, setValues] = useState(new Array(9).fill('').map((cur) => ({ content: '', condition: '' })));
+  // const handleReset = () => {
+  //   setValues(new Array(9).fill('').map((cur) => ({ content: '', condition: '' })));
+  //   setTurn(0);
+  //   setWins([]);
+  // };
+
+  const init = () => {
+    return { 
+      turn: 0,
+      values: new Array(9).fill('').map((cur) => ({ content: '', condition: '' })),
+      wins: [],
+    };
+  };
+
+
+  // const [values, setValues] = useState(new Array(9).fill('').map((cur) => ({ content: '', condition: '' })));
   const players = ['X', 'O'];
-  const [turn, setTurn] = useState(0);
-  const [wins, setWins] = useState([]);
+  // const [turn, setTurn] = useState(0);
+  // const [wins, setWins] = useState([]);
   const [dimensions, ref] = useComponentSize();
+
+  const reducer = (state, action) => {
+    switch (action.type){
+    case 'move':
+      console.log('action', action);
+      const i = action.idx;
+      if (state.values[i].content || state.turn >= players.length) { return state; }
+
+      const newValues = state.values.map((cur, idx) => (i === idx ? { ...cur, content: players[state.turn] } : { ...cur }));
+      const newWins = state.wins.map(cur => { return { ...cur }; });
+      let newTurn = (state.turn + 1) % players.length;
+
+      for (let i = 0; i < 3; i++) {
+        if (players[state.turn] === newValues[i * 3].content && players[state.turn] === newValues[i * 3 + 1].content && players[state.turn] === newValues[i * 3 + 2].content) {
+          newTurn = players.length + state.turn;
+          newWins.push({ key: 'row', x1: 1 / 9, y1: 1 / 6 + i / 3, x2: 8 / 9, y2: 1 / 6 + i / 3 });
+          newValues[i * 3].condition = newValues[i * 3 + 1].condition = newValues[i * 3 + 2].condition = 'win';
+        }
+        if (players[state.turn] === newValues[i].content && players[state.turn] === newValues[i + 3].content && players[state.turn] === newValues[i + 6].content) {
+          newTurn = players.length + state.turn;
+          newWins.push({ key: 'cols', x1: 1 / 6 + i / 3, y1: 1 / 9, x2: 1 / 6 + i / 3, y2: 8 / 9 });
+          newValues[i].condition = newValues[i + 3].condition = newValues[i + 6].condition = 'win';
+        }
+      }
+      if (players[state.turn] === newValues[0].content && players[state.turn] === newValues[4].content && players[state.turn] === newValues[8].content) {
+        newTurn = players.length + state.turn;
+        newWins.push({ key: 'slash', x1: 1 / 9, y1: 1 / 9, x2: 8 / 9, y2: 8 / 9 });
+        newValues[0].condition = newValues[4].condition = newValues[8].condition = 'win';
+      }
+      if (players[state.turn] === newValues[2].content && players[state.turn] === newValues[4].content && players[state.turn] === newValues[6].content) {
+        newTurn = players.length + state.turn;
+        newWins.push({ key: 'backslash', x1: 8 / 9, y1: 1 / 9, x2: 1 / 9, y2: 8 / 9 });
+        newValues[2].condition = newValues[4].condition = newValues[6].condition = 'win';
+      }
+      if (newValues.filter((cur) => cur.content).length === 9 && state.turn < players.length) {
+        newTurn = players.length * 2;
+      }
+
+      // setWins(newWins);
+      // setValues(newValues);
+      // setTurn(newTurn);
+      return {wins: newWins, values: newValues, turn: newTurn};
+    case 'reset':
+      console.log('reset', action);
+      return init();
+    case 'default':
+      console.log('default', action);
+      return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, init());
 
   const handleKeyPress = (event) => {
     const row = 3;
@@ -90,77 +163,69 @@ const Game = () => {
     };
   }, []);
 
-  const move = (i) => {
-    if (values[i].content || turn >= players.length) { return; }
-    const newValues = values.map((cur, idx) => (i === idx ? { ...cur, content: players[turn] } : { ...cur }));
-    const newWins = wins.map(cur => {return {...cur}; });
-    let newTurn = (turn + 1) % players.length;
+  // const move = (i) => {
+  //   if (values[i].content || turn >= players.length) { return; }
+  //   const newValues = values.map((cur, idx) => (i === idx ? { ...cur, content: players[turn] } : { ...cur }));
+  //   const newWins = wins.map(cur => {return {...cur}; });
+  //   let newTurn = (turn + 1) % players.length;
     
-    for (let i = 0; i < 3; i++) {
-      if (players[turn] === newValues[i * 3].content && players[turn] === newValues[i * 3 + 1].content && players[turn] === newValues[i * 3 + 2].content) {
-        newTurn = players.length + turn;
-        newWins.push( { key: 'row', x1: 1 / 9, y1: 1/6 + i/3, x2: 8/9, y2: 1/6 + i/3 } );
-        newValues[i*3].condition = newValues[i*3 + 1].condition = newValues[i*3 + 2].condition = 'win';
-      }
-      if (players[turn] === newValues[i].content && players[turn] === newValues[i + 3].content && players[turn] === newValues[i + 6].content) {
-        newTurn = players.length + turn;
-        newWins.push( { key: 'cols', x1: 1/6 + i/3, y1: 1 / 9, x2: 1/6 + i/3, y2: 8/9 } );
-        newValues[i].condition = newValues[i + 3].condition = newValues[i + 6].condition = 'win';
-      }
-    }
-    if (players[turn] === newValues[0].content && players[turn] === newValues[4].content && players[turn] === newValues[8].content) {
-      newTurn = players.length + turn;
-      newWins.push({ key: 'slash', x1: 1 / 9, y1: 1 / 9, x2: 8/9, y2: 8/9 } );
-      newValues[0].condition = newValues[4].condition = newValues[8].condition = 'win';
-    }
-    if (players[turn] === newValues[2].content && players[turn] === newValues[4].content && players[turn] === newValues[6].content) {
-      newTurn = players.length + turn;
-      newWins.push({ key: 'backslash', x1: 8/9, y1: 1/9, x2: 1/9, y2: 8/9 } );
-      newValues[2].condition = newValues[4].condition = newValues[6].condition = 'win';
-    }
-    if (newValues.filter((cur) => cur.content).length === 9 && turn < players.length) {
-      newTurn = players.length * 2;
-    }
+  //   for (let i = 0; i < 3; i++) {
+  //     if (players[turn] === newValues[i * 3].content && players[turn] === newValues[i * 3 + 1].content && players[turn] === newValues[i * 3 + 2].content) {
+  //       newTurn = players.length + turn;
+  //       newWins.push( { key: 'row', x1: 1 / 9, y1: 1/6 + i/3, x2: 8/9, y2: 1/6 + i/3 } );
+  //       newValues[i*3].condition = newValues[i*3 + 1].condition = newValues[i*3 + 2].condition = 'win';
+  //     }
+  //     if (players[turn] === newValues[i].content && players[turn] === newValues[i + 3].content && players[turn] === newValues[i + 6].content) {
+  //       newTurn = players.length + turn;
+  //       newWins.push( { key: 'cols', x1: 1/6 + i/3, y1: 1 / 9, x2: 1/6 + i/3, y2: 8/9 } );
+  //       newValues[i].condition = newValues[i + 3].condition = newValues[i + 6].condition = 'win';
+  //     }
+  //   }
+  //   if (players[turn] === newValues[0].content && players[turn] === newValues[4].content && players[turn] === newValues[8].content) {
+  //     newTurn = players.length + turn;
+  //     newWins.push({ key: 'slash', x1: 1 / 9, y1: 1 / 9, x2: 8/9, y2: 8/9 } );
+  //     newValues[0].condition = newValues[4].condition = newValues[8].condition = 'win';
+  //   }
+  //   if (players[turn] === newValues[2].content && players[turn] === newValues[4].content && players[turn] === newValues[6].content) {
+  //     newTurn = players.length + turn;
+  //     newWins.push({ key: 'backslash', x1: 8/9, y1: 1/9, x2: 1/9, y2: 8/9 } );
+  //     newValues[2].condition = newValues[4].condition = newValues[6].condition = 'win';
+  //   }
+  //   if (newValues.filter((cur) => cur.content).length === 9 && turn < players.length) {
+  //     newTurn = players.length * 2;
+  //   }
 
-    setWins(newWins);
-    setValues(newValues);
-    setTurn(newTurn);
-  };
+  //   setWins(newWins);
+  //   setValues(newValues);
+  //   setTurn(newTurn);
+  // };
 
-  const extras = wins.map(cur => {
-    let width = dimensions.width - 20;
-    let height = dimensions.height -20;
 
-    return (<Line 
-      key={cur.key}
-      x1={ width * cur.x1 + 5} // 1/3 for rows and slashes i + 0.5 for col
-      y1={ height * cur.y1 + 5} // 1/3 for cols and slashes i + 0.5 for row
-      x2={ width * cur.x2 + 5} 
-      y2={ height * cur.y2 + 5} 
-      color={'lightgreen'} />);
-  });
+  console.log(state);
 
-  const squares = values.map((cur, idx) => (
+  // const extras = state.wins.map(cur => {
+  //   let width = dimensions.width - 20;
+  //   let height = dimensions.height -20;
+
+  //   return (<Line 
+  //     key={cur.key}
+  //     x1={ width * cur.x1 + 5}
+  //     y1={ height * cur.y1 + 5}
+  //     x2={ width * cur.x2 + 5} 
+  //     y2={ height * cur.y2 + 5} 
+  //     color={'lightgreen'} />);
+  // });
+  const extras = [];
+
+  const squares = state.values.map((cur, idx) => (
     <Square
       idx={idx}
       content={cur.content}
       condition={cur.condition}
       key={idx}
-      handleClick={() => move(idx)}
+      handleClick={() => dispatch({type: 'move', idx: idx})}
     />
   ));
-
-  const handleReset = () => {
-    setValues(new Array(9).fill('').map((cur) => ({ content: '', condition: '' })));
-    setTurn(0);
-    setWins([]);
-  };
-
-  const getMessage = (players, turn) => {
-    if (players[turn]) { return `Player ${players[turn]}'s turn.`; }
-    if (players[turn - players.length]) { return `${players[turn - players.length]} wins!!!`; }
-    return 'Tie Game';
-  };
 
   return (
     <div
@@ -171,7 +236,7 @@ const Game = () => {
         border: 'none',
       }}
     >
-      <GameInfo message={getMessage(players, turn)} handleReset={handleReset} />
+      <GameInfo message={getMessage(players, state.turn)} handleReset={()=>dispatch({type:'reset'})} />
       <Board squares={squares} extras={extras} boardRef={ref}/>
     </div>
   );
